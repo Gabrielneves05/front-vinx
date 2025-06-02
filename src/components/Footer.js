94f617d1647e109c61c174572dee8cbf0b2548fd
Gabrielneves05 <gabrielneves05@outlook.com>
2025-06-02 20:04:58 -0300

    chore: creation of initial navbar and footer structures

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import "./Footer.css";
+
+const Footer = () => {
+    return (
+        <footer id="footer">
+            <div id="footer-content">
+                <p>Vinx &copy; 2022</p>
+                <p>Todos os direitos reservados</p>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
