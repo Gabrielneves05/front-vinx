e7e00cc1877d08ca9a4de89edd298859434565d4
Gabrielneves05 <gabrielneves05@outlook.com>
2025-06-02 23:31:39 -0300

    styles: registration form creation / form styling / global application styles

--- src/pages/Auth/Register.js
+++ src/pages/Auth/Register.js
@@ -1,8 +1,29 @@
 import "./Auth.css";
 
+// Components
+import { Link } from "react-router-dom";
+
+// Hooks
+import { useState, useEffect } from "react";
+
 const Register = () => {
+    const handleSubmit = e => {
+        e.preventDefault();
+    }
+
     return (
-        <div>Register</div>
+        <div className="auth-container">
+            <h1>VinX</h1>
+            <p className="subtitle">Crie sua conta</p>
+            <form onSubmit={handleSubmit}>
+                <input type="text" placeholder="Nome" />
+                <input type="email" placeholder="E-mail" />
+                <input type="password" placeholder="Senha" />
+                <input type="password" placeholder="Confirme sua senha" />
+                <input type="submit" value="Cadastrar" />
+            </form>
+            <p>Já possui uma conta? <Link to="/login">Entrar</Link></p>
+        </div>
     )
 }
 
