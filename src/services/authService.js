91ce6c9d3f69ff2105a551c0f86221638a9b1410
Gabrielneves05 <gabrielneves05@outlook.com>
2025-06-10 15:12:34 -0300

    feat: creating an authentication service

---
+++ src/services/authService.js
@@ -0,0 +1,24 @@
+import { api, requestConfig } from '../utils/config';
+
+// Register an user
+const register = async (data) => {
+    const config = requestConfig("POST", data);
+    
+    try {
+        const response = await fetch(api + "/users/register", config)
+            .then(res => res.json())
+            .catch(err => err);
+
+        if(response) {
+            localStorage.setItem("user", JSON.stringify(response));
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const authService = {
+    register
+}
+
+export default authService;
