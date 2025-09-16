4a63747ee80f2b33c973e5a86ed799d366edf0ca
Gabrielneves05 <gabrielneves05@outlook.com>
2025-09-16 12:13:30 -0300

    feat: implementation of photo editing function in photo slice

--- src/slices/photoSlice.js
+++ src/slices/photoSlice.js
@@ -39,6 +39,27 @@ export const getUserPhotos = createAsyncThunk(
     }
 )
 
+// Update a photo
+export const updatePhoto = createAsyncThunk(
+    "photo/update",
+    async(photoData, thunkAPI) => {
+        const token = thunkAPI.getState().auth.user.token;
+
+        const data = await photoService.updatePhoto(
+            { title: photoData.title }, 
+            photoData.id, 
+            token
+        );
+
+        // Check for errors
+        if(data.errors) {
+            return thunkAPI.rejectWithValue(data.errors[0]);
+        }
+
+        return data;
+    }
+)
+
 // Delete a photo
 export const deletePhoto = createAsyncThunk(
     "photo/delete",
@@ -124,6 +145,36 @@ export const photoSlice = createSlice({
         })
         .addCase(
             deletePhoto.rejected, 
+            (state, action) => {
+                state.loading = false;
+                state.error = action.payload;
+                state.photo = {};
+        })
+        .addCase(
+            updatePhoto.pending, 
+            state => {
+                state.loading = true;
+                state.error = false;
+        })
+        .addCase(
+            updatePhoto.fulfilled, 
+            (state, action) => {
+                state.loading = false;
+                state.success = true;
+                state.error = null;
+
+                state.photos.map(photo => {
+                    if(photo._id === action.payload.photo._id) {
+                        return photo.title = action.payload.photo.title;
+                    }
+
+                    return photo;
+                })
+
+                state.message = action.payload.message;
+        })
+        .addCase(
+            updatePhoto.rejected, 
             (state, action) => {
                 state.loading = false;
                 state.error = action.payload;
