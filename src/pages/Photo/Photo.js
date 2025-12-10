71402e5930e0b764cc8dd5dd83a546167ec87071
Gabrielneves05 <gabrielneves05@outlook.com>
2025-12-10 13:59:16 -0300

    feat: implementation of comment functionality

--- src/pages/Photo/Photo.js
+++ src/pages/Photo/Photo.js
@@ -14,7 +14,7 @@ import { useParams } from "react-router-dom";
 import { useResetComponentMessage } from "../../hooks/useResetComponentMessage";
 
 // Redux
-import { getPhoto, like } from "../../slices/photoSlice";
+import { getPhoto, like, comment } from "../../slices/photoSlice";
 import LikeContainer from "../../components/LikeContainer/LikeContainer";
 
 const Photo = () => {
@@ -28,14 +28,32 @@ const Photo = () => {
     const { photo, loading, error, message } = useSelector(state => state.photo);
 
     // Comments
+    const [commentText, setCommentText] = useState("");
 
     // Load photo data
     useEffect(() => {
         dispatch(getPhoto(id));
     }, [dispatch, id]);
 
+    // Insert a like
     const handleLike = () => {
         dispatch(like(photo._id));
+
+        resetMessage();
+    }
+
+    // Insert a comment
+    const handleComment = (e) => {
+        e.preventDefault();
+
+        const commentData = {
+            comment: commentText,
+            id: photo._id
+        }
+
+        dispatch(comment(commentData));
+
+        setCommentText("");
 
         resetMessage();
     }
@@ -57,6 +75,42 @@ const Photo = () => {
             <div className="message-container">
                 {error && <Message message={error} type="error" />}
                 {message && <Message message={message} type="success" />}
+            </div>
+            <div className="comments">
+                {photo.comments && (
+                    <>
+                        <h3>Comentários: ({photo.comments.length})</h3>
+                        <form onSubmit={handleComment}>
+                            <input 
+                                type="text" 
+                                placeholder="Insira o seu comentário..." onChange={(e) => setCommentText(e.target.value)} 
+                                value={commentText || ""} 
+                            />
+                            <input 
+                                type="submit" 
+                                value="Enviar" 
+                            />
+                        </form>
+                        {photo.comments.length === 0 && <p>Não há comentários ainda.</p>}
+                        {photo.comments.map((comment) => (
+                            <div className="comment" key={comment.comment}>
+                                <div className="author">
+                                    {comment.userImage && (
+                                        <img 
+                                            src={`${uploadUrl}/users/${comment.userImage}`} 
+                                            alt={comment.userName} 
+                                        />
+                                    )}
+
+                                    <Link to={`/users/${comment.userId}`}>
+                                        <p>{comment.userName}</p>
+                                    </Link>
+                                </div>
+                                <p>{comment.comment}</p>
+                            </div>
+                        ))}
+                    </>
+                )}
             </div>
         </div>
     )
